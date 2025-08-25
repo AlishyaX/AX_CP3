8635b303f776e07fa6fa502e1a945bf2abe52771
AlishyaX <alishya.xavier@ucas-edu.net>
2025-08-25 11:47:49 -0600

    strings notes

---
+++ Notes/strings_arrays_con.cpp
@@ -0,0 +1,47 @@
+// AX Strings, Arrays, Conditionals notes
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+    int seconds = time(nullptr);
+    srand(seconds);
+    int my_num = rand() % 11;
+    cout << my_num;
+    return 0;
+}
+
+
+
+//Questions:
+//------------------------------------------------------------------------------------
+// What is Narrowing?
+    // you take a variable that already exists and move its content to a variable that is a smaller amount of space.
+    // for example chnaging a long into a int
+    // Risk: You can loose info
+    // With numbers you can use apostrophes as commas and it wont mess up anything
+
+// What is a basic way to generate random numbers in C++?
+    //
+
+// What is an array?
+    //
+
+// How do I create an array?
+    //
+
+// How do you make strings in C?
+    //
+
+// How did C++ improve creating strings? 
+    //
+
+// How do I search a string?
+    //
+
+// How do I modify a string?
+    //
+
+// What are some string methods? 
+    //
