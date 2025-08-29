acd3d634f47703484133909b5e144fa0a74cf691
AlishyaX <alishya.xavier@ucas-edu.net>
2025-08-29 11:41:33 -0600

    made notes

--- Notes/arrays_parttwo.cpp
+++ Notes/arrays_parttwo.cpp
@@ -4,6 +4,7 @@
 #include <limits>
 using namespace std;
 
+
 string fam[5] = {"Alishya", "Andrew", "Jalaja", "Ajash"};
 string sibs[1] = {"Ajash"};
 // Array as an argument
@@ -36,9 +37,9 @@ int main(){
     cout << e << endl;
 
     //Multi demensional
-    int matrix[3][3] = {{1,2,3},
-                        {4,5,6},
-                        {7,8,9}};
+    int matrix[3][3][2] = {{{1,2},{1,2},{1,2}}
+                    {{1,2},{1,2},{1,2}},
+                    {{1,2},{1,2},{1,2}}};
 
     return 0;
 }
@@ -63,15 +64,43 @@ int main(){
 
 //What is type_t
     // finds the size of the largest type of info
+    
 
 //Why would we use type_t
     // you can use it as a replacement for specifying data
+    // We use it to tell when we don't know how much we are going to need
 
 //How do you search an array
-    //
+    //std::find can help you find things in an array?
 
 //How do you sort an array
-    //
+    // Bubble sort: takes two numbers and then compares them, it takes the two numbers and checks which one is smallest and keps going to check 2 numbers
+    // Then you repeat it again and again until its sorted from least to greatest
+    // 0. length of the loop
+    // 1. Loop array
+    // 2. select second item 
+    // 3. Compare it to first item
+    // 4. if i-1 is bigger switch them
+
+int nums[] = {5, 24, 1, 0, 66, 47, 3}; 
+int main(){
+    int length = size(nums);
+    for(int j = 0; j<length; j++){
+        for(int i=1; i < (length); i++){
+            //cout << nums[i] << endl;
+            if(nums[i] < nums[i-1]){
+                int swap = nums[i];
+                nums[i] = nums[i-1];
+                nums[i-1] = swap;
+            }
+        }
+    }
+    for(int i = 0; i < length; i++){
+        cout << nums[i] << ", ";
+    }
+    return 0;
+}
+
 
 //How do you make a multi-dimensional array
-    //
+    //Do matrix [# of rows][# of columns] = {{}{}{}}
