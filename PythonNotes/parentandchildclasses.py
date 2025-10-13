5df99195a5dadd0e12135bc64d25749deeb31b3f
AlishyaX <alishya.xavier@ucas-edu.net>
2025-10-13 11:47:45 -0600

    started with notes

---
+++ PythonNotes/parentandchildclasses.py
@@ -0,0 +1,35 @@
+# AX Parent and Child Classes Notes
+
+'''
+
+QUESTIONS:
+What is a parent/abstract class?
+a box with the name of the class at the top, attributes in the middle, and methods at the bottom, and 
+the child classes point to it with a clear arrow.
+
+How do you create a child class?
+it is a box with the name of the class at the top, attributes in the middle, and methods at the bottom 
+and a clear arrow pointing to the parent class.
+
+How does a child class inherit from the parent class?
+
+
+What are class diagrams?
+they allow us to visualize what we put in our classes
+
+How are class diagrams used to show a parent/child relationship?
+
+
+How do you overload operators in a class?
+
+
+What are test cases?
+
+
+Why do we use test cases?
+
+
+How do we create test cases?
+
+
+'''
